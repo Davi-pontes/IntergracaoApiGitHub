2c61c4b0c07716d4286b627e9280471f870bed63
Davi Jose <davimartins2450@gmail.com>
2023-03-26 10:00:05 -0300

    API github

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const { default: axios } = require('axios')
 const express = require('express')
 
 const app = express()
 
 app.listen('8080')
 
-app.route('/').get((req,res) => res.send("Hello"))
+app.route('/').get ((req,res) => {
+
+    axios.get('https://api.github.com/users/Davi-pontes')
+    .then(result => res.send(`<img src="${result.data.avatar_url}"/>`))
+    .catch(error => console.error(error))
+})
